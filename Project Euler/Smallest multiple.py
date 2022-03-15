323bb18239946ea90f18a5e79511f3d3d55451e2
ilteberkonuralp <ilteber.konuralp65@gmail.com>
2022-03-15 16:50:27 +0300

    Calculation of Least Common Multiple was made with fundamental algorithm however it can be improved.

--- Project Euler/Smallest multiple.py
+++ Project Euler/Smallest multiple.py
@@ -2,3 +2,30 @@
 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"""
+import time
+time1=time.time()
+def max_of_two(x,y):
+    if x>y:
+        return x
+    elif y>x:
+        return y
+    else:
+        return x
+def calculate_least_common_multiple(x=20):
+    a,b=1,2
+    num=max_of_two(a,b)
+    while(a<=x):
+
+        while(True):
+            if((num%a==0)and(num%b==0)):
+                a,b=a+1,num
+                break 
+            else:
+                num+=1
+                 
+        
+    return num
+print(calculate_least_common_multiple())        
+time2=time.time()
+timedif=round(time2-time1,3)
+print(f"Elapsed time to calculation : {timedif}")
