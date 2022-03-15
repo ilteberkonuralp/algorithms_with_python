7e34d5e37ec33313ae5a964e2ed4e241ddc566c0
ilteberkonuralp <ilteber.konuralp65@gmail.com>
2022-03-15 08:59:55 +0300

    two mathematical question solutions and problem set need to solve

---
+++ Project Euler/Sum square difference.py
@@ -0,0 +1,10 @@
+"""The sum of the squares of the first ten natural numbers is,
+                sum((1:10)^2)=385
+The square of the sum of the first ten natural numbers is,
+                    sum(1:10)^2=3025
+Hence the difference between the sum of the squares of the first ten natural numbers 
+and the square of the sum is .
+                3025-385=2640
+Find the difference between the sum of the squares of the first one hundred natural
+ numbers and the square of the sum."""
+
