5a63d9580cad7bfc2593511c7ba1bf5a3be6612d
ilteberkonuralp <ilteber.konuralp65@gmail.com>
2022-09-19 22:51:12 +0300

    Three traversal type was added.

--- ALGORITHMS AND DATA STRUCTURES WITH PYTHON/BinaryTree.py
+++ ALGORITHMS AND DATA STRUCTURES WITH PYTHON/BinaryTree.py
@@ -5,18 +5,42 @@ class Node:
         self.right = None
         self.data = value
 
-if __name__=="__main__":
-    root=Node(5)
 
-    root.left=Node(3)
-    root.right=Node(4)
-    root.left.left=Node(2)
-    print(root.left.data)
-    # Depth First Traversals
-    def Inorder():
-        pass
-    def Preorder():
-        pass
-    def Postorder():
-        pass
+root=Node(1)
+root.left=Node(2)
+root.right=Node(3)
+root.left.left=Node(4)
+root.left.right=Node(5)
+root.right.left=Node(6)
+root.right.right=Node(7)
+# print(root.left.data)
+# Depth First Traversals
+def InorderTraversal(root):
+    
+    if root is None:
+        return
+    InorderTraversal(root.left)
+    print(root.data,end=" ")
+    
+    InorderTraversal(root.right)
 
+print("Inorder Traversal result:")
+InorderTraversal(root)
+
+def PreorderTraversal(root):
+    if root is None:
+        return
+    print(root.data,end=" ")
+    PreorderTraversal(root.left)
+    PreorderTraversal(root.right)
+print("\nPreorder Traversal result:")
+PreorderTraversal(root)
+def PostorderTraversal(root):
+    if root is None:
+        return
+    
+    PreorderTraversal(root.left)
+    PreorderTraversal(root.right)
+    print(root.data,end=" ")
+print("\nPost order Traversal result:")
+PostorderTraversal(root)
