31708bd61e6da4c2a5804fb94a0dff23490a5710
ilteberkonuralp <ilteber.konuralp65@gmail.com>
2023-01-29 20:54:59 +0300

    Binary Tree was changed which will be extend after learning new informations.

--- ALGORITHMS AND DATA STRUCTURES WITH PYTHON/BinaryTree.py
+++ ALGORITHMS AND DATA STRUCTURES WITH PYTHON/BinaryTree.py
@@ -1,10 +1,10 @@
 # First Create binary node
 class Node:
-    def __init__(self, value):
+    def __init__(self, value:int):
+        
         self.left = None
         self.right = None
         self.data = value
-
 
 root=Node(1)
 root.left=Node(2)
@@ -15,17 +15,16 @@ root.right.left=Node(6)
 root.right.right=Node(7)
 # print(root.left.data)
 # Depth First Traversals
-def InorderTraversal(root):
-    
+def InorderTraversal(root: Node) -> None:
     if root is None:
         return
     InorderTraversal(root.left)
     print(root.data,end=" ")
     
     InorderTraversal(root.right)
-
-print("Inorder Traversal result:")
-InorderTraversal(root)
+    return
+# print("Inorder Traversal result:")
+# InorderTraversal(root)
 
 def PreorderTraversal(root):
     if root is None:
@@ -33,8 +32,8 @@ def PreorderTraversal(root):
     print(root.data,end=" ")
     PreorderTraversal(root.left)
     PreorderTraversal(root.right)
-print("\nPreorder Traversal result:")
-PreorderTraversal(root)
+# print("\nPreorder Traversal result:")
+# PreorderTraversal(root)
 def PostorderTraversal(root):
     if root is None:
         return
@@ -42,5 +41,5 @@ def PostorderTraversal(root):
     PreorderTraversal(root.left)
     PreorderTraversal(root.right)
     print(root.data,end=" ")
-print("\nPost order Traversal result:")
-PostorderTraversal(root)
+# print("\nPost order Traversal result:")
+# PostorderTraversal(root)
